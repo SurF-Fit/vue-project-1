9fc21a22c9dc1a2b20ff9dd9bc5dc2cd295c295a
SurF-Fit <kkulmenev@gmail.com>
2025-02-03 14:32:20 +0700

    Practical work No. 7

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ let app = new Vue({
     el: '#app',
     data:{
         product: "Socks",
+        brand: 'Vue Mastery',
         allText: "A pair of socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        inStock: true,
+        onSale: true,
         inventory: 100,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -14,15 +14,18 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
+        selectedVariant: 0,
     },
     methods: {
         addToCart() {
@@ -32,8 +35,23 @@ let app = new Vue({
             if (this.cart === 0) this.cart = 0
             else this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            return this.brand + ' ' + this.product + ' ' + "sale"
+        }
     },
 })
