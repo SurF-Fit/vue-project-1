822f2194ebab73e64308003f41d2b3e0742afaf1
SurF-Fit <kkulmenev@gmail.com>
2025-02-03 13:37:19 +0700

    Practical work No. 4

--- js/main.js
+++ js/main.js
@@ -6,8 +6,19 @@ let app = new Vue({
         product: "Socks",
         allText: "A pair of socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory: 100,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
