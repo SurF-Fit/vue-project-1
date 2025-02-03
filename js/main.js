954ce4659f2af94d90247001e884d7ed3d6bd9a7
SurF-Fit <kkulmenev@gmail.com>
2025-02-03 13:16:31 +0700

    Practical work No. 2

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ let app = new Vue({
     el: '#app',
     data:{
         product: "Socks",
-        description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-green-onWhite.jpg"
+        allText: "A pair of socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
     }
 })
